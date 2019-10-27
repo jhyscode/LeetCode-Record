d7284c3d424761ae38e33e812992561e528af2a3
jhys <1770613507@qq.com>
2019-10-27 15:43:44 +0800

    add leetocde21

--- src/com/leetcode/_21/Solution.java
+++ src/com/leetcode/_21/Solution.java
@@ -70,11 +70,11 @@ public class Solution {
 
     public static void main(String[] args) {
         ListNode l1 = new ListNode(1);
-        l1.next = new ListNode(3);
-        l1.next.next = new ListNode(5);
-        ListNode l2 = new ListNode(2);
-        l2.next = new ListNode(4);
-        l2.next.next = new ListNode(6);
+        l1.next = new ListNode(1);
+        l1.next.next = new ListNode(2);
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
         ListNode result = new Solution().mergeTwoLists1(l1, l2);
         System.out.println(result);
     }
