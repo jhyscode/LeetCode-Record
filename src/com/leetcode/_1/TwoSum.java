2867fc74d3139ac3f7f3b7724f1de28c17388dee
jhys <1770613507@qq.com>
2020-11-11 20:06:35 +0800

    update

--- src/com/leetcode/_1/TwoSum.java
+++ src/com/leetcode/_1/TwoSum.java
@@ -1,5 +1,10 @@
 package com.leetcode._1;
 
+import java.net.Inet4Address;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Problem1
  * @author jhys
@@ -17,9 +22,26 @@ public class TwoSum {
         }
         return null;
     }
+
+    public static int[] twoSum1(int[] nums, int target) {
+        int res[] = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+                return res;
+            }
+            else {
+                map.put(nums[i], i);
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        twoSum(nums, target);
+        int target = 13;
+        twoSum1(nums, target);
     }
 }
---
+++ src/com/leetcode/_23/Solution.java
@@ -0,0 +1,14 @@
+package com.leetcode._23;
+
+/**
+ * @author ：jhys
+ * @date ：Created in 2020/11/11 20:02
+ * @Description ：
+ */
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+    }
+}
